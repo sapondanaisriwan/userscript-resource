055f533cee9156f0aa3b833ef0d1b70350e446eb
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2023-05-07 00:28:04 +0700

    Create waitForElement.js

---
+++ waitForElement.js
@@ -0,0 +1,19 @@
+function waitForElement(selector) {
+  return new Promise((resolve) => {
+    const element = document.querySelector(selector);
+    if (element) {
+      resolve(element);
+      return;
+    }
+
+    const observer = new MutationObserver(() => {
+      const element = document.querySelector(selector);
+      if (element) {
+        resolve(element);
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, config);
+  });
+}
